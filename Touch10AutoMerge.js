8359a965583c0be44c4f33c03bbaf11bda040ece
Tor Martin Ølberg <toolb@equinor.com>
2020-11-30 15:58:56 +0100

    Added check for CUCM
    Since we dont want to try and merge if a mediaresourcergrouplist is avaliable a check for that was introduced so that the endpoint dosent try to join the calls...

--- Touch10AutoMerge.js
+++ Touch10AutoMerge.js
@@ -2,21 +2,38 @@ const xapi = require('xapi');
 
 var NumCall = 0; 
 var LastDirection = "";
+var MultiPointMode = "";
+
+
 
 function AutoMerge()
   {
-    console.log("Automerge comencing, checking if call is outgoing");
-    console.log("Number of calls is: " + NumCall);
-    if (LastDirection === "Outgoing" && NumCall === 2)
+    //console.log("Automerge comencing, checking if call is outgoing");
+    //console.log("Number of calls is: " + NumCall);
+    console.log("MultipointMode is: " + MultiPointMode);
+    if (MultiPointMode === "CUCMMediaResourceGroupList")
     {
-      xapi.command('Call Join');
+      if (LastDirection === "Outgoing" && NumCall === 2)
+      {
+        xapi.command('Call Join');
+      }
     }
-    
+    else
+    {
+      console.log("Endpoint not registered on CUCM or no resourcelist has been defined, not merging...");
+    }
+  }
+
+function ConfigState(State)
+  {
+    if (State === "Auto" || State === "CUCMMediaResourceGroupList" || State == "Multisite" || State == "Off")
+      {
+        MultiPointMode = State;
+      }
   }
 
 xapi.status.on('Call', InCallStatus =>
   {
-    console.log(InCallStatus);
     if (InCallStatus.Direction === "Outgoing" || InCallStatus.Direction === "Incoming")
     {
       LastDirection = InCallStatus.Direction;
@@ -27,6 +44,7 @@ xapi.status.on('Call', InCallStatus =>
 xapi.status.on('SystemUnit State NumberOfActiveCalls', NumCalls =>
   {
     NumCall = Number.parseInt(NumCalls);
+    xapi.status.get('Conference Multipoint Mode').then(ConfigState);
     if (NumCall === 2)
       {
         AutoMerge();
